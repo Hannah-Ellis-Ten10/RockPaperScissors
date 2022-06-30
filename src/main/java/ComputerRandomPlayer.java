dab6f89c307bc5281ba20b5b5d901397645352df
Hannah Ellis <hannah.ellis@centre4.local>
2022-06-30 11:00:49 +0100

    adding a player interface and adding a ComputerRandomPlayer that implements that interface

---
+++ src/main/java/ComputerRandomPlayer.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class ComputerRandomPlayer implements Player{
+    Random rng;
+    ComputerRandomPlayer(){
+        rng = new Random();
+    }
+    @Override
+    public String getName() {
+        return "Computer";
+    }
+
+    @Override
+    public Choice getChoice() {
+        Choice[] choices = Choice.values();
+        int randomIndex = rng.nextInt(choices.length);
+        return choices[randomIndex];
+    }
+
+    @Override
+    public void addOppoentChoice(Choice choice) {
+        //Do nothing, we don't care what out opponent did
+    }
+
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,5 @@
+public interface Player {
+    String getName();
+    Choice getChoice();
+    void addOppoentChoice(Choice choice);
+}

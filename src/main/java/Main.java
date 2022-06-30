a71be1786d4692e889d6d1fd0ca99c80be50eee3
Hannah Ellis <hannah.ellis@centre4.local>
2022-06-30 11:04:09 +0100

    using players in current version of game and making a loop so we ensure there's always a winner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     static public void main(String[] args){
         Game game = new Game();
-        GameOutcome outcome = game.play(Choice.ROCK,Choice.PAPER);
+        Player player1 = new ComputerRandomPlayer();
+        Player player2 = new ComputerRandomPlayer();
+        GameOutcome outcome;
+        do {
+            outcome = game.play(player1.getChoice(), player2.getChoice());
+        }while(outcome == GameOutcome.DRAW);
         System.out.println(outcome);
     }
 }
